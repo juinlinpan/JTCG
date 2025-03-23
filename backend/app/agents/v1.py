f6ba8177423bf7a2673d9e11e0bd47c0ea4853e7
laura <evilauraaa@gmail.com>
2025-03-23 21:36:04 +0800

    [add] V1agent新增planner

--- backend/app/agents/v1.py
+++ backend/app/agents/v1.py
@@ -10,6 +10,16 @@ from langchain_core.messages import (
     SystemMessage,
     ToolMessage,
 )
+
+from pydantic import BaseModel, Field
+
+from langchain_core.tools import tool
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate
+)   
+
 os.environ['GOOGLE_API_KEY'] = 'AIzaSyC8lJWeRoheKe6QBReAfRbhiried6EqChc'
 
 
@@ -24,19 +34,55 @@ SYS_RESP = """
     - 人數
     - 預算
 2. 用戶提供齊全後，將用戶需求交由"行程安排師"進行行程安排
+
+* Rundown:
+1. 先跟她介紹你可以為他安排行程，與他打招呼。
+2. 逐一詢問用戶，不要一次問，這樣有壓迫感。
+3. 用戶可能會用不同的方式提供資訊，要會解讀成上述四個項目。
+4. 一旦蒐集到四個項目，就統整用戶需求並覆誦一次，與用戶確認是否想要進一步安排行程。
+5. 確認完畢後，不用回覆用戶，直接呼叫"行程安排師"進行行程安排。
+6. 行程規劃師會從他的電腦回覆後下線，請繼續服務用戶
+7. 用戶如果更改行程需求，則再次從 2. 開始，或可能再次呼叫"行程安排師"。
+
+* Rules:
+1. 用戶不知道"行程安排師"的存在，所以不要提及"行程安排師"。
+2. 用戶確認無誤之後不要先回覆，直接呼叫"行程安排師"。
+3. 全程使用繁體中文
 """
 
-SYS_PLAN = """
+@tool
+def call_planner(requirement: str):
+    """
+    呼叫這個函數，會將需求(requirement)傳遞給行程安排師，並等待行程安排師的回覆
+    等到與用戶蒐集到足夠資訊後，使用此函數。
+    回傳即是行程安排師的回覆。
+    """
+
+SYS_PLANNER = """
 你是一位旅遊推薦平台的行程安排師
-你的工作是負責為用戶提供旅遊住宿與周邊探索的整合解決方案
+你的工作是負責為用戶提供旅遊住宿與周邊探索的整合解決方案。
+你會收到用戶的需求，請根據此需求為用戶安排行程。
+
+* Rules:
+1. 輸出要可讀性高
 """
+
+HUMAN_PLANNER = "需求: {requirement}"
 
 
 class V1Agent():
     def __init__(self, event_queue):
         self.event_queue = event_queue
         self.messages = []
-        self.llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.5)
+        self.llm_resp = ChatGoogleGenerativeAI(
+            model="gemini-2.0-flash",
+            temperature=0.5
+            ).bind_tools([call_planner])
+        
+        self.llm_planner = ChatGoogleGenerativeAI(
+            model="gemini-2.0-flash",
+            temperature=0.5
+            )
 
     def reset_messages(self):
         self.messages = []
@@ -51,16 +97,34 @@ class V1Agent():
         self.messages.append(user_message)
         langchain_messages = [SystemMessage(content=SYS_RESP)]
         langchain_messages += [self.convert_message_to_langchain_message(msg) for msg in self.messages]
-        output = self.llm.invoke(langchain_messages)
-        self.messages.append(self.convert_langchain_message_to_message(output))
-
-        await self.event_queue.put({
-            "data": json.dumps({
-                "type": "plan_completed",
-                "timestamp": time.time()
+        output = self.llm_resp.invoke(langchain_messages)
+        if len(output.content) > 0:
+            self.messages.append(self.convert_langchain_message_to_message(output))
+            await self.event_queue.put({
+                "data": json.dumps({
+                    "type": "plan_completed",
+                    "timestamp": time.time()
+                })
             })
-        })
-        
+        else:
+            print(output.tool_calls)
+            requirement = output.tool_calls[0]['args']['requirement']
+            ret = "收到您的需求，請稍等片刻，馬上為您安排行程"
+            ai_message = schemas.MessageContent(
+                content=ret,
+                role="assistant"
+            )
+            self.messages.append(ai_message)
+            
+            await self.event_queue.put({
+                "data": json.dumps({
+                    "type": "plan_completed",
+                    "timestamp": time.time()
+                })
+            })
+            self.planning_task = asyncio.create_task(self.plan(requirement))
+
+
     def convert_message_to_langchain_message(self, msg):
         if msg.role == "assistant":
             langchain_message = AIMessage(content=msg.content)
@@ -80,5 +144,26 @@ class V1Agent():
         return self.messages
     
     
-    
-    
+    async def plan(self, requirement: str):
+        # 此方法模擬背景計算過程
+        print("開始執行計劃任務...")
+        
+        prompt_template = ChatPromptTemplate(
+            messages=[
+                SystemMessagePromptTemplate.from_template(SYS_PLANNER),
+                HumanMessagePromptTemplate.from_template(HUMAN_PLANNER)
+            ]
+        )
+        prompt = prompt_template.format_prompt(requirement=requirement)
+        output = self.llm_planner.invoke(prompt)
+        self.messages.append(self.convert_langchain_message_to_message(output))
+        
+        # 發送事件通知
+        await self.event_queue.put({
+            "data": json.dumps({
+                "type": "plan_completed",
+                "timestamp": time.time()
+            })
+        })
+        
+        print("計劃任務執行完畢")
